fb5a41035855a3f85f70f9634832f430bb711886
RuiPenetra <2180680@my.ipleiria.pt>
2019-11-11 21:38:28 +0000

    Inicio do projeto Android e seus Mockups

---
+++ Android/AplicAndroid/GestAlunos/app/src/main/java/amsi/dei/estg/ipleiria/pt/gestalunos/MenuActivity.java
@@ -0,0 +1,15 @@
+package amsi.dei.estg.ipleiria.pt.gestalunos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+}
