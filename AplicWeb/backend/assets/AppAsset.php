3399a7209d062f5ad6b16dcc3e8d2905aba46b69
Shider <simao.shider@gmail.com>
2019-11-25 12:21:13 +0000

    Update AplicWeb
    Reiniciar

--- AplicWeb/backend/assets/AppAsset.php
+++ AplicWeb/backend/assets/AppAsset.php
@@ -12,23 +12,12 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/bootstrap.min.css',
-        'css/sidebar.css',
-        'css/datatable.css',
         'css/site.css',
     ];
     public $js = [
-      'js/font-awesome.js',
-      'js/jquery.min.js',
-      'js/datatable.js',
-      'js/popper.min.js',
-      'js/jquery.easing.min.js',
-      'js/bootstrap.min.js',
-      'js/jquerydatatable.js',
-      'js/Chart.min.js',
-      'js/tether.min.js',
-      'js/backoffice.js',
     ];
     public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
     ];
 }
