4688b0f45ad2c7f7c62c80309c2c891d3c6b185e
Shider <simao.shider@gmail.com>
2019-12-01 03:38:40 +0000

    Header Sidebar e Home page

--- AplicWeb/backend/views/layouts/main.php
+++ AplicWeb/backend/views/layouts/main.php
@@ -9,6 +9,8 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
+use yii\web\View;
+
 
 AppAsset::register($this);
 ?>
@@ -23,96 +25,166 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
+<?php
+  if(!Yii::$app->user->isGuest) {
+?>
+<body class="hold-transition skin-blue fixed hold-transition login-page">
+<?php }else { ?>
+<body class="bg-light-blue color-palette">
+<?php } ?>
 <?php $this->beginBody() ?>
+  <?php
+    if(!Yii::$app->user->isGuest) {
+  ?>
+    <div class="wrapper">
+        <header class="main-header">
+          <!-- Logo -->
+          <a href="index2.html" class="logo">
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg">GestorAlunos</span>
+          </a>
+          <!-- Header Navbar: style can be found in header.less -->
+          <nav class="navbar navbar-static-top">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+              <span class="sr-only">Toggle navigation</span>
+            </a>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+            <div class="navbar-custom-menu">
+              <ul class="nav navbar-nav">
+                <!-- Messages: style can be found in dropdown.less-->
+                <li class="dropdown messages-menu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="label label-success">0</span>
+                  </a>
+                  <ul class="dropdown-menu">
+
+                  </ul>
+                </li>
+                <!-- Notifications: style can be found in dropdown.less -->
+                <li class="dropdown notifications-menu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-bell-o"></i>
+                    <span class="label label-warning">0</span>
+                  </a>
+                  <ul class="dropdown-menu">
+
+                  </ul>
+                </li>
+                <!-- Tasks: style can be found in dropdown.less -->
+                <li class="dropdown tasks-menu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-flag-o"></i>
+                    <span class="label label-danger">0</span>
+                  </a>
+                  <ul class="dropdown-menu">
+
+                  </ul>
+                </li>
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <?= Html::img('@web/img/businessman.png', ['alt'=>'imgPerfil', 'class'=>'user-image']); ?>
+                    <span class="hidden-xs">Nome de Pessoa</span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <!-- User image -->
+                    <li class="user-header">
+                      <?= Html::img('@web/img/businessman.png', ['alt'=>'imgPerfil', 'class'=>'img-circle']); ?>
+                      <p>
+                        Nome de Pessoa
+                        <small>Professor</small>
+                      </p>
+                    </li>
+                    <!-- Menu Body -->
+                    <li class="user-body">
+                        <div class="btn-group-vertical btn-block">
+                          <a href="" class="btn btn-info"><i class="fa fa-user"></i> Perfil</a>
+                          <a href="" class="btn btn-info"><i class="fa fa-gear fa-spin"></i> Configurações Conta</a>
+                          <a href="" class="btn btn-info"><i class="fa fa-user-cog"></i> Configurações Perfil</a>
+                          <a href="#" class="btn btn-info"><i class="fa fa-sign-out"></i> Sair</a>
+                        </div>
+                      <!-- /.row -->
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+          <div class="user-panel">
+            <div class="pull-left image">
+              <?= Html::img('@web/img/businessman.png', ['alt'=>'imgPerfil', 'class'=>'img-circle']); ?>
+            </div>
+            <div class="pull-left info">
+              <p>Nome de Pessoa</p>
+              <a href="#"><i class="fa fa-id-card text-success"></i> Professor</a>
+            </div>
+          </div>
+          <!-- /.search form -->
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">Nav</li>
+            <li class="active">
+              <a href="#">
+                <i class="fa fa-home"></i> <span>Home</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-user"></i> <span>Alunos</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-book"></i> <span>Disciplinas</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-book"></i> <span>Cursos</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-calendar"></i> <span>Horário</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="#">
+                <i class="fa fa-book"></i> <span>Testes</span>
+              </a>
+            </li>
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
+      <div class="content-wrapper">
+        <section class="content">
+          <?= Breadcrumbs::widget([
+              'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+          ]) ?>
+          <?= Alert::widget() ?>
+          <?= $content ?>
+        </section>
+        </div>
+      </div>
+  <?php
+    }else{
+  ?>
+      <?= Breadcrumbs::widget([
+          'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+      ]) ?>
+      <?= Alert::widget() ?>
+      <?= $content ?>
+  <?php
     }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-
+  ?>
 <?php $this->endBody() ?>
 </body>
 </html>
--- AplicWeb/backend/views/site/login.php
+++ AplicWeb/backend/views/site/login.php
@@ -8,28 +8,53 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="login-box">
+  <div class="login-logo">
+    <b>Gestor</b>Alunos
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sistema de Autenticação</p>
 
-    <p>Please fill out the following fields to login:</p>
+    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+        <?= $form->field($model, 'username', ['options'=>['tag'=>'div', 'class'=>'form-group field-loginform-username has-feedback require'], 'template'=>'{input}<span class="glyphicon glyphicon-envelope form-control-feedback"></span>{error}{hint}'])->textInput(['placeholder'=>'Email']) ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="row">
+          <div class="col-xs-8">
+            <?= $form->field($model, 'rememberMe')->checkbox()->label('Lembrar-me') ?>
+          </div>
+          <div class="col-xs-4">
+            <?= Html::submitButton('Entrar', ['class' => 'btn btn-info btn-block btn-flat', 'name' => 'login-button']) ?>
+          </div>
         </div>
-    </div>
+        <div class="row">
+          <br>
+          <br>
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <a class="btn btn-info btn-block" href="#">
+              <i class="fa fa-mobile fa-2x"></i><br> Entrar pelo Smartphone
+            </a>
+          </div>
+        </div>
+
+    <?php ActiveForm::end(); ?>
+
+  </div>
+  <!-- /.login-box-body -->
 </div>
+
+
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
