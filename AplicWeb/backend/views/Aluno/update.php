978ee0683a2be0bd1c9369a9f2e7b5dd9026e32d
Shider <simao.shider@gmail.com>
2019-12-06 00:18:26 +0000

    ca

--- AplicWeb/backend/views/Aluno/update.php
+++ AplicWeb/backend/views/Aluno/update.php
@@ -1,7 +1,10 @@
 <?php
 
 use yii\helpers\Html;
-
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+use backend\models\Aluno;
+use backend\models\Perfil;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Aluno */
 
@@ -9,13 +12,19 @@ $this->title = 'Update Aluno: ' . $model->id_perfil;
 $this->params['breadcrumbs'][] = ['label' => 'Alunos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id_perfil, 'url' => ['view', 'id' => $model->id_perfil]];
 $this->params['breadcrumbs'][] = 'Update';
+
+
 ?>
 <div class="aluno-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?php $form = ActiveForm::begin(); ?>
+
+      <?= $form->field($model, 'id_perfil'); ?>
+
+      <?= $form->field($model, 'id_perfil')->dropDownList(ArrayHelper::map(Perfil::find()->where(['id' => 123]),'id_perfil',), ['prompt'=>'Seleciona']) ; ?>
+
+    <?php ActiveForm::end(); ?>
 
 </div>
