7e44130b64f65d5851c1aedaf60fdf7c4e276649
Dinas69 <leopoldo31920@gmail.com>
2019-12-11 00:52:03 +0000

    Login quase feito - Questionar prof

--- AplicWeb/backend/views/site/login.php
+++ AplicWeb/backend/views/site/login.php
@@ -6,10 +6,12 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use backend\assets\AppAssetLogin;
+use yii\helpers\Url;
 
 AppAssetLogin::register($this);
 
 $this->title = 'Login';
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="bodyClass">
     <img class="wave" src="img/wave.png">
@@ -18,81 +20,47 @@ $this->title = 'Login';
             <a href="https://www.ipleiria.pt/" class="aClass"><img src="img/bg.svg" ></a>
         </div>
         <div class="login-content">
-            <form class="formClass">
 
-                <?php $form = ActiveForm::begin(['id' => 'login-form', 'class' => 'formClass']); ?>
-                <img src="img/ipl.png">
-                <h2 class="title"></h2>
-                <div class="input-div one">
-                    <div class="iClass">
-                        <i class="fas fa-user"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Email</h5>                        
-                        <?= $form->field($model, 'username', ['options' => ['tag' => 'input', 'class' => 'input require'], 'template' => '{input}<span class="glyphicon glyphicon-envelope form-control-feedback"></span>{error}{hint}']) ?>
-                    </div>
-                </div>
-                <div class="input-div pass">
-                    <div class="iClass">
-                        <i class="fas fa-lock"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Password</h5>
-                        <input type="password" class="input">
-                        <?= $form->field($model, 'password', ['options' => ['tag' => 'input', 'class' => 'input require'], 'template' => '{input}<span class="glyphicon glyphicon-envelope form-control-feedback"></span>{error}{hint}'])->passwordInput() ?>
-                    </div>
-                </div>
-                <a href="#" class="aClass">Esqueceu a senha?</a>
-                <?= Html::submitButton('Entrar', ['class' => 'btnClass', 'name' => 'login-button']) ?>
+            <!-- <?//php
+            $form = ActiveForm::begin([
+            'id' => 'login-form',
+            'class' => 'formClass',
+            'action' => Url::to(['/site/index'])
+            ]);
+            ?>-->
 
-                <?php ActiveForm::end(); ?>
-            </form>
-        </div>
-    </div>
-    <div class="bodyClass">
-        <img class="wave" src="img/wave.png">
-        <div class="containerClass">
-            <div class="imgClass">
-                <a href="https://www.ipleiria.pt/" class="aClass"><img src="img/bg.svg" ></a>
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <img src="img/ipl.png">
+            <h2 class="title"></h2>
+            <div class="input-div one">
+                <div class="iClass">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="div">
+                    <h5>Email</h5>            
+                    <?= $form->field($model, 'username', ['options' => ['tag' => 'input', 'style' => 'display: none;']])->textInput(['class' => 'input'])->label(false) ?>     
+             <!--<?//= $form->field($model, 'username', ['options' => ['tag' => 'input', 'class' => 'input require']])->label('') ?>-->
+                </div>
             </div>
-            <div class="login-content">
-                <form class="formClass" action="index.html">
-                    <img src="img/ipl.png">
-                    <h2 class="title"></h2>
-                    <div class="input-div one">
-                        <div class="iClass">
-                            <i class="fas fa-user"></i>
-                        </div>
-                        <div class="div">
-                            <h5>Email</h5>
-                            <input type="text" class="input">
-                        </div>
-                    </div>
-                    <div class="input-div pass">
-                        <div class="iClass">
-                            <i class="fas fa-lock"></i>
-                        </div>
-                        <div class="div">
-                            <h5>Password</h5>
-                            <input type="password" class="input">
-                        </div>
-                    </div>
-                    <a href="#" class="aClass">Esqueceu a senha?</a>
-                    <input type="submit" class="btnClass" value="Entrar">
-                </form>
+            <div class="input-div pass">
+                <div class="iClass">
+                    <i class="fas fa-lock"></i>
+                </div>
+                <div class="div">
+                    <h5>Password</h5>
+                    <?= $form->field($model, 'password', ['options' => ['tag' => 'input', 'style' => 'display: none;']])->textInput(['class' => 'input', 'type' => 'password'])->label(false) ?>
+                    <!--<?/= $form->field($model, 'password', ['options' => ['tag' => 'input', 'class' => 'input require'],])->passwordInput()->label('') ?>-->
+                </div>
             </div>
+            <a href="#" class="aClass">Esqueceu a senha?</a>
+            <div class="form-group">
+                <?= Html::submitButton('Login', ['class' => 'btnClass', 'name' => 'login-button']) ?>
+            </div>
+                     <!--<?/= Html::submitButton('Entrar', ['class' => 'btnClass', 'name' => 'login-button']) ?>-->
+
+             <!--<?/php ActiveForm::end(); ?>-->
+
+            <?php ActiveForm::end(); ?>
         </div>
-        <script type="text/javascript" src="js/main.js"></script>
     </div>
 </div>
-
-
-<script>
-    $(function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' /* optional */
-        });
-    });
-</script>
