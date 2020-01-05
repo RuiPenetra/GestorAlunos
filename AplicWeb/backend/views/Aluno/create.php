a66e07516c5bfd0277cadedf813af81774c1b5e1
Dinas69 <leopoldo31920@gmail.com>
2020-01-05 00:50:12 +0000

    Correção de repetição de nome
    Eliminar o nome dos alunos já criados, ou seja, não aparecer no dropdown nome aluno os alunos que ja estao inseridos na tabela.
    ERRO DE SINTAXE!!

--- AplicWeb/backend/views/Aluno/create.php
+++ AplicWeb/backend/views/Aluno/create.php
@@ -5,6 +5,7 @@ use yii\bootstrap\ActiveForm;
 use yii\helpers\ArrayHelper;
 use backend\models\Curso;
 use backend\models\Perfil;
+use backend\models\Aluno;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Aluno */
@@ -16,12 +17,20 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="aluno-create">
 
     <h1><?php Html::encode($this->title) ?></h1>
-
+    <!--//->where('not like','id_user')-->
     <?php $form = ActiveForm::begin(); ?>
-        <?= $form->field($model, 'id_perfil')->dropDownList(ArrayHelper::map(Perfil::find()->all(), 'id_user','nome'), ['prompt'=>'Selecione uma Opção']) ?>
-        <?= $form->field($model, 'id_curso')->dropDownList(ArrayHelper::map(Curso::find()->all(), 'id','nome'), ['prompt'=>'Selecione uma Opção']) ?>
-        <?= Html::submitButton('Criar', ['class' => 'btn btn-primary']) ?>
-    <?php ActiveForm::end(); ?>
+    <?=
+    $form->field($model, 'id_perfil')->dropDownList(ArrayHelper::map(Perfil::find()
+                            ->where(['not like', 'id_user', (ArrayHelper::map(Aluno::find()
+                                        ->all(), 'id_perfil', 'id_curso'))])
+                            ->all(), 'id_user', 'nome'), ['prompt' => 'Selecione uma Opção'])
+    ?>
+    <?=
+    $form->field($model, 'id_curso')->dropDownList(ArrayHelper::map(Curso::find()
+                            ->all(), 'id', 'nome'), ['prompt' => 'Selecione uma Opção'])
+    ?>
+    <?= Html::submitButton('Criar', ['class' => 'btn btn-primary']) ?>
+<?php ActiveForm::end();?>
 
 
 
