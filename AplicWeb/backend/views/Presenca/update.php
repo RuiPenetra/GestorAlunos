4a932ad21070a576d0f5241b31070c5a8997c8fb
RuiPenetra <2180680@my.ipleiria.pt>
2020-01-14 22:56:29 +0000

    Registo faltas aluno no professor

--- AplicWeb/backend/views/Presenca/update.php
+++ AplicWeb/backend/views/Presenca/update.php
@@ -1,21 +1,26 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Presenca */
 
-$this->title = 'Update Presenca: ' . $model->id;
+$this->title = 'Atualizar Falta ';
 $this->params['breadcrumbs'][] = ['label' => 'Presencas', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = ['label' => $model->perfil->perfil->nome, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Atualizar';
 ?>
 <div class="presenca-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'id_aula')->dropDownList(ArrayHelper::map($aulas, 'id','nome','dia'), ['prompt' => 'Selecione uma Opção']) ?>
+        <?= $form->field($model, 'data',['inputOptions' => ['id' => 'datepicker', 'class' => 'form-control','autocomplete' => 'off']]); ?>
+        <?= $form->field($model, 'id_perfil')->dropDownList(ArrayHelper::map($alunos, 'id_perfil','perfil.nome'), ['prompt'=>'Selecione']) ?>
+        <?= Html::submitButton('Atualizar', ['class' => 'btn btn-primary']) ?>
+    <?php ActiveForm::end(); ?>
 
 </div>
